a6ccf1aee9042197a33c4e9cf0b1edb7a5728afd
EmmaRodR <emmanuelrgeek@gmail.com>
2024-10-24 21:16:09 -0300

    headers config

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -48,9 +48,11 @@ export const loginUser = async (data: LoginData): Promise<LoginResponse> => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': 'true',
         Authorization: authHeader(),
       },
       body: JSON.stringify(data),
+      credentials: 'include'
     });
 
 
