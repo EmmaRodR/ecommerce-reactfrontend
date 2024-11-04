c401f559ac8d6f205e517284905422d3b30eebef
EmmaRodR <emmanuelrgeek@gmail.com>
2024-11-04 15:14:57 -0300

    change api path

--- src/services/api-consts.ts
+++ src/services/api-consts.ts
@@ -1,4 +1,4 @@
-export const LOCAL_BASE_URL = 'http://localhost:8080'
+export const LOCAL_BASE_URL = 'https://ecommerce-springboot-backend.onrender.com'
 export const REGISTER_ENDPOINT_URL =`${LOCAL_BASE_URL}/auth/register`
 export const LOGIN_ENDPOINT_URL =`${LOCAL_BASE_URL}/auth/authenticate`
 export const PRODUCTS_BASE_URL =`${LOCAL_BASE_URL}/api/v1/products`
