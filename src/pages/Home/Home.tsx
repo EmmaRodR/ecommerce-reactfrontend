fa8f3a91e3850fb928a5460963f3098574162701
EmmaRodR <emmanuelrgeek@gmail.com>
2024-11-09 21:12:39 -0300

    assets

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,10 +10,10 @@ const Home: React.FC = () => {
           With an authentication and security system using JWT with Spring Boot Security, complemented by a web frontend built in React and CSS using TypeScript, without additional libraries. 
           </p>
           <div className="icons-container">
-           <div className="individual-icon-container"><img src="src/assets/java-svgrepo-com.svg" width="50" alt="" /></div>
-           <div className="individual-icon-container"><img src="src/assets/spring-boot.svg" width="50" alt="" /></div>
-           <div className="individual-icon-container"><img src="src/assets/icons8-react-48.png"width={50} alt="" /></div>
-           <div className="individual-icon-container"><img src="src/assets/icons8-typescript-50.png" alt="" /></div>
+           <div className="individual-icon-container"><img src="/public/icons8-java-50.png" alt="" /></div>
+           <div className="individual-icon-container"><img src="/public/spring-boot.svg" width="50" alt="" /></div>
+           <div className="individual-icon-container"><img src="/public/icons8-react-48.png"width={50} alt="" /></div>
+           <div className="individual-icon-container"><img src="/public/icons8-typescript-50.png" alt="" /></div>
           </div>
         </div>
       </div>
