102110f4501c11ac5b183990e6a4490343c179dd
EmmaRodR <emmanuelrgeek@gmail.com>
2024-11-11 21:07:48 -0300

    dale

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -27,5 +27,4 @@ const Home: React.FC = () => {
 };
 
 
-
 export default Home;
--- src/services/api-consts.ts
+++ src/services/api-consts.ts
@@ -2,7 +2,7 @@ export const LOCAL_BASE_URL = 'https://ecommerce-springboot-backend.onrender.com
 export const REGISTER_ENDPOINT_URL =`${LOCAL_BASE_URL}/auth/register`
 export const LOGIN_ENDPOINT_URL =`${LOCAL_BASE_URL}/auth/authenticate`
 export const PRODUCTS_BASE_URL =`${LOCAL_BASE_URL}/api/v1/products`
-export const CATEGORIES_BASE_URL =`${LOCAL_BASE_URL}/api/v1/categories`
+export const CATEGORIES_BASE_URL =`${LOCAL_BASE_URL}/api/v1/catWegories`
 export const CART_BASE_URL =`${LOCAL_BASE_URL}//api/v1/categories`
 
 
